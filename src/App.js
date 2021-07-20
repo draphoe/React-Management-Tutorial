dd734fe605153d70cdb8633f78cf3c901715ef58
draphoe <draphoe@hanmail.net>
2021-07-20 23:13:22 +0900

    Create customers.js

--- src/App.js
+++ src/App.js
@@ -2,16 +2,55 @@ import React from 'react';
 import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Customer from './components/customer'
+
+const customers=[{
+  id: 1,
+  image : 'https://placeimg.com/64/64/any',
+  name: '홍길동',
+  DoB: '961222',
+  gender: 'Male',
+  job: '대학생'
+},
+{
+  id: 2,
+  image : 'https://placeimg.com/64/64/any',
+  name: 'MoonLight',
+  DoB: '711222',
+  gender: 'Male',
+  job: 'Killer'
+},
+{
+  id: 3,
+  image : 'https://placeimg.com/64/64/any',
+  name: 'Draphoe',
+  DoB: '206122',
+  gender: 'Female',
+  job: 'Assasinator'
+} ]
 
 class App extends React.Component {
   render(){
     return (
-      <div className="grey-background">
-        <img src={logo} lat="logo"/>
-        <h2>Let's develop management system!</h2>
-      </div>      
+      <div>
+        { customers.map(c=>{
+            return(
+              <Customer
+                key={c.id} 
+                id={c.id} 
+                image={c.image} 
+                name={c.name} 
+                gender={c.gender} 
+                DoB={c.DoB} 
+              />
+            )
+          })
+        }
+      </div>  
+        
     );  
   }
 }
 
 export default App;
+
