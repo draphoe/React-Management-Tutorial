e68e2a614cdb73bdf287a44e43fe2ce1b5b1c512
draphoe <draphoe@hanmail.net>
2021-07-21 00:18:43 +0900

    Create material-ui

---
+++ src/components/customer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+
+class Customer extends React.Component{
+  render(){
+    return(
+      <TableRow>
+        <TableCell>{this.props.id}</TableCell>
+        <TableCell><img src={this.props.image} alt="profiles"/></TableCell>
+        <TableCell>{this.props.name}</TableCell>
+        <TableCell>{this.props.gender}</TableCell>
+        <TableCell>{this.props.DoB}</TableCell>
+        <TableCell>{this.props.job}</TableCell>
+      </TableRow>
+
+      
+    );
+  }
+}
+
+export default Customer;
